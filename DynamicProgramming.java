b001b7e8ce460b8357bae2293ef0a53db76c4ec3
peteradelmakram <peteradelmakram@gmail.com>
2022-11-20 10:13:56 +0200

    Done.

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -5,7 +5,6 @@ public class DynamicProgramming {
         long[] f = new long[n+2];
         f[0] = 0;
         f[1] = 1;
-        
         //Create an array with base cases, iterate over n times, add previous two using O(n) duhh!
         for(int i = 2; i <= n; i++){
             f[i] = f[i-1] + f[i-2]; 
@@ -14,7 +13,6 @@ public class DynamicProgramming {
         return f[n];
     }
 
-    //THIS IS THE COOLEST FUCKING SHIT EVER
     public static long fib(int n){
         Map<Integer, Long> DP = new HashMap<>(); // Map (fib(3) --> 2 / fib(4) --> 3 / fib(5) --> 5 / fib(6) --> 8)
         return fibDynamic(n, DP);
@@ -34,20 +32,39 @@ public class DynamicProgramming {
         return badFib(n-1) + badFib(n-2);
 
     }
-    public static void main(String[] args) {
-        System.out.print(fib(6));
+    public int tribonacci(int n) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(0,0);
+        dp.put(1,1);
+        dp.put(2,1);
+        return tribonacci(n, dp);
+    }
+    public int tribonacci(int n, Map<Integer,Integer> dp){
+        if(dp.containsKey(n)) return dp.get(n);
+        
+        dp.put(n, (tribonacci(n-1, dp) + tribonacci(n-2, dp) + tribonacci(n-3, dp)));
+        
+        return dp.get(n);
     }
 
-// fib(3) --> 2
-// fib(4) --> 3
+    public static long gridTraveler(int m, int n){
+        Map<String, Long> DP = new HashMap<>();
+        return gridTraveler(m, n, DP);
+    }
+    public static long gridTraveler(int m, int n , Map<String,Long> DP){
+        String key = m + "," + n;
+        if(DP.containsKey(key)) return DP.get(key);
+        if(m == 1 && n == 1) return 1;
+        if(m == 0 || n == 0) return 0;
 
+        DP.put(key, gridTraveler(m-1, n, DP) + gridTraveler(m, n-1, DP));
 
-    /*                
-            
-        
-        
-        
-        
-                            
-     */ 
+        return DP.get(key);
+    }
+    
+    public static void main(String[] args) {
+        System.out.print(gridTraveler(18,18));
+    }
+
+                
 }
--- GUC/Labs/ArrayStack.java
+++ GUC/Labs/ArrayStack.java
@@ -1,3 +1,4 @@
+
 public class ArrayStack {
     private int[] stackArray;
     private int top;
